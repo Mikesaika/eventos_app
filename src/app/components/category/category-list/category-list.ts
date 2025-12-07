37e6d84ab375fd7b6b4408e8ebb70389529cf9ed
JosuePita1203 <josue.pitaf@ug.edu.ec>
2025-12-07 17:57:27 +0000

    material eliminado

--- src/app/components/category/category-list/category-list.ts
+++ src/app/components/category/category-list/category-list.ts
@@ -2,51 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../../services/category.service';
 import { Category } from '../../../models/category';
 import { Router } from '@angular/router';
-import { NotificationComponent } from '../../shared/notification/notification.component';
-import { TableComponent } from '../../shared/table/table.component';
+
+import { TableReutilizable } from '../../../shared/table-reutilizable/table-reutilizable';
+import { NotificationComponent } from '../../../shared/notification/notification';
 
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [NotificationComponent, TableComponent],
-  templateUrl: './category-list.component.html'
+  imports: [
+    TableReutilizable,
+    NotificationComponent
+  ],
+  templateUrl: './category-list.html'
 })
 export class CategoryListComponent implements OnInit {
 
   categories: Category[] = [];
-  notificationMessage = '';
-  notificationType: 'success' | 'error' = 'success';
-
   constructor(
     private service: CategoryService,
     private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.load();
-  }
-
-  load() {
-    this.service.list().subscribe({
-      next: (data) => this.categories = data,
-      error: () => {
-        this.notificationMessage = 'Error cargando categorías.';
-        this.notificationType = 'error';
-      }
+    this.service.list().subscribe((data: Category[]) => {
+      return this.categories = data;
     });
-  }
-
-  newCategory() {
-    this.router.navigate(['/categories/new']);
-  }
-
-  editCategory(id: string) {
-    this.router.navigate(['/categories/edit', id]);
-  }
-
-  deleteCategory(id: string) {
-    this.categories = this.categories.filter(c => c.id !== id);
-    this.notificationMessage = 'Categoría eliminada localmente.';
-    this.notificationType = 'success';
   }
 }
